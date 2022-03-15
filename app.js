fe7f28a8486df73830c2bec28b66685f5d94aa4c
diehard5566 <shihyao001@gmail.com>
2022-03-15 19:31:09 +0800

    app set up

--- app.js
+++ app.js
@@ -1,1 +1,33 @@
-console.log('04 Store API')
+require('dotenv').config();
+// async error
+
+const express = require('express');
+const app = express();
+
+const notFoundMiddleware = require('./middleware/not-found');
+const errorMiddleware = require('./middleware/error-handler');
+
+// middleware
+app.use(express.json())
+
+//routes
+app.get('/', (req, res) => {
+    res.send(`<h1>Store</h1><a href="/api/v1/products">products</a>`)
+})
+
+//product route
+
+app.use(notFoundMiddleware);
+
+const port = process.env.PORT || 3000
+
+const start = async () => {
+    try {
+        //connectDB
+        app.listen(port, console.log(`Server is listening port ${port}`));
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+start()
